18307cc30332aa5a47cee90d0b2d04bc9df7bd3a
santiagogozu <santiagogo1993@gmail.com>
2023-01-04 23:11:13 -0500

    CSS Tablet and onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import magnifying from './magnifying.png';
-import './App.css';
 
 function App() {
+  
+
+  const onClickButton = () => {
+    console.log("holaaa")
+    return 1
+  };
+
   return (
     <div className="App">
+      <div className="prueba">
       <header className="App-header">
         <form action="">
           <input name="" id="" cols="30" rows="10" className='txtEncript'></input>
-          <div class="buttons">
-            <input type="submit" value="Encriptar" class="btnEncript"></input>
-            <input type="submit" value="Desencriptar" class="btnDesencript"></input>
+          <div className="buttons">
+            <button className="btnEncript" onClick={onClickButton}><h1>Encriptar</h1></button>
+            <button className="btnDesencript"><h1>Desencriptar</h1></button>
           </div>
         </form>
         <div className="desencriptador">
-          <img src={magnifying} className="imgNot" alt="logo" />
+          <img src={magnifying} className='imgNot' alt="logo" />
+          {/* <img src={magnifying} className={`${(onClickButton===1) ? 'imgNot' : 'imgNot2'}`} alt="logo" /> */}
           <div className='txtNotDeco'>
               <h1> Ningún mensaje fue encontrado.</h1>
               <h2>Ingresa el texto que desees encriptar o desencriptar.</h2>
@@ -21,6 +29,7 @@ function App() {
         </div>
 
       </header>
+      </div>
     </div>
   );
 }
